328c109e4968393986492b5e23f8878fbb59d0bf
Rongjun GENG <rj.geng@gmail.com>
2024-08-25 06:56:04 -0700

    setup a Emily's Trinkets project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+
+import data from '../data'
+
+function fetchStock() {
+  // fetchStock simulates getting data through axios.get(<URL>)
+  return Promise.resolve({ success: true, data })
+}
 
 export const  App =() => {
-  return (<>
-      <h1>Hello world!</h1>
-    </>);
-};
+  const [stock, setStock] = useState([])
+
+  useEffect(() => {
+    fetchStock().then(res => setStock(res.data))
+  }, [])
+
+  return (
+    <div className='App'>
+      <nav>
+        <h1 className='store-header'>Emily&apos;s Trinkets</h1>
+        <div className='nav-links'>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
+import './styles/index.less'
 
 const container = document.getElementById("root");
 const root = createRoot(container)
